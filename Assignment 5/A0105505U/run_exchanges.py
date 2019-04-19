6c5ee8345beff4087e78f3ce2bf517334f663a2a
xiaogouman <xiaogouman@hotmail.com>
2019-04-19 17:53:38 +0800

    save

---
+++ Assignment 5/A0105505U/run_exchanges.py
@@ -0,0 +1,57 @@
+import argparse
+from sqlalchemy.orm import sessionmaker
+import time
+from db_connect import get_conn
+import random
+
+## Argument parser to take the parameters from the command line
+## Example on how to run: python run_exchanges.py 10 READ_COMMITTED
+parser = argparse.ArgumentParser()
+parser.add_argument('E', type = int, help = 'number of exchange transactions in a process')
+parser.add_argument('I', help = 'isolation level')
+args = parser.parse_args()
+
+
+def exchange(sess):
+    # generate two random account number
+    account_nums = random.sample(range(1, 100000), 2)
+
+    balance_0 = float(list(sess.execute(" \
+    SELECT balance from account \
+    where id = " + str(account_nums[0])))[0][0])
+
+    balance_1 = float(list(sess.execute(" \
+    SELECT balance from account \
+    where id = " + str(account_nums[1])))[0][0])
+
+    sess.execute(" UPDATE account set balance = " + str(balance_1) + " \
+    where id = " + str(account_nums[0]))
+
+    sess.execute(" UPDATE account set balance = " + str(balance_0) + " \
+    where id = " + str(account_nums[1]))
+
+## Create S sums operations
+def E_swaps(sess, E):
+    start = time.time()
+
+    for i in xrange(0, E):
+        while True:
+            try:
+                exchange(sess)
+                sess.commit()
+                time.sleep(0.0001)
+            except Exception as e:
+                # print e
+                sess.rollback()
+                continue
+            break
+
+    stop = time.time()
+    return stop-start
+
+## Create the engine and run the sums
+engine = get_conn()
+Session = sessionmaker(bind=engine.execution_options(isolation_level=args.I, autocommit=True))
+sess = Session()
+time = E_swaps(sess, args.E)
+print time
--- Assignment 5/project5/run_and_plot.py
+++ Assignment 5/project5/run_and_plot.py
@@ -1,57 +1,109 @@
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
-file_names = ["READ UNCOMMITTED.csv", "READ COMMITED.csv", "REPEATABLE READ.csv", "SERIALIZABLE.csv"]
+file_names = ["READ UNCOMMITTED.csv", "READ COMMITTED.csv", "REPEATABLE READ.csv", "SERIALIZABLE.csv"]
 
 
-file_name = file_names[1]
-ps = []
-times = []
-corrects = []
+for file_name in file_names:
+    ps = []
+    times = []
+    corrects = []
 
-with open(file_name) as f:
-    csv_reader = csv.reader(f, delimiter=',')
-    for row in csv_reader:
-        ps.append(int(row[1]))
-        corrects.append(float(row[3]))
-        times.append(float(row[4]))
+    with open(file_name) as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        for row in csv_reader:
+            ps.append(int(row[1]))
+            corrects.append(float(row[3]))
+            times.append(float(row[4]))
 
-print len(times)
-print len(corrects)
+    print len(times)
+    print len(corrects)
 
-p_corrects = []
-p_times = []
-distinct_ps = []
-for i in range(len(ps)/20):
-    start = i*20
-    end = (i+1)*20
-    p_corrects.append(corrects[start:end])
-    p_times.append(times[start:end])
-    distinct_ps.append(ps[start])
+    p_corrects = []
+    p_times = []
+    distinct_ps = []
+    user_correct_mean = []
+    user_time_mean = []
+    for i in range(len(ps)/20):
+        start = i*20
+        end = (i+1)*20
+        p_corrects.append(corrects[start:end])
+        p_times.append(times[start:end])
+        distinct_ps.append(ps[start])
+        user_correct_mean.append(np.average(corrects[start:end]))
+        user_time_mean.append(np.average(times[start:end]))
 
-# print avg_times
-# print avg_corrects
-print np.asarray(p_corrects)
+    type = file_name.replace(".csv", "")
+    p_corrects = np.asarray(p_corrects).T
+    p_times = np.asarray(p_times).T
+    distinct_ps = np.asarray(distinct_ps)
 
-def draw_plot(data, offset,edge_color, fill_color):
-    pos = np.arange(data.shape[1])+offset
-    bp = ax.boxplot(data, positions= pos, widths=0.3, patch_artist=True, manage_xticks=False)
-    for element in ['boxes', 'whiskers', 'fliers', 'medians', 'caps']:
-        plt.setp(bp[element], color=edge_color)
-    for patch in bp['boxes']:
-        patch.set(facecolor=fill_color)
+    # fig, ax = plt.subplots()
+    # bp = ax.boxplot(np.asarray(p_corrects).T, usermedians=user_correct_mean, whis="range")
+    # ax.set_xticklabels(distinct_ps)
+    # ax.set_xlabel("Number of subprocesses")
+    # ax.set_ylabel("Correctness")
+    # ax.set_title(type)
+    # plt.savefig(file_name.replace(".csv", "-correctness.png"))
+    #
+    # fig, ax2 = plt.subplots()
+    # bp2 = ax2.boxplot(np.asarray(p_times).T, usermedians=user_time_mean, whis="range")
+    # ax2.set_xticklabels(distinct_ps)
+    # ax2.set_xlabel("Number of subprocesses")
+    # ax2.set_ylabel("Time(s)")
+    # ax2.set_title(type)
+    # plt.savefig(file_name.replace(".csv", "-time.png"))
+
+    # mins = p_corrects.min(0)
+    # maxes = p_corrects.max(0)
+    # means = p_corrects.mean(0)
+    # std = p_corrects.std(0)
+    #
+    # # create stacked errorbars:
+    # plt.errorbar(np.arange(11), means, std, fmt='ok', lw=3)
+    # plt.errorbar(np.arange(11), means, [means - mins, maxes - means],
+    #              fmt='.k', ecolor='gray', lw=1)
+    # plt.xlim(-1, 11)
+
+    x = p_times
+    mins = x.min(0)
+    maxes = x.max(0)
+    means = x.mean(0)
+    std = x.std(0)
+
+    # create stacked errorbars:
+    fig, ax = plt.subplots()
+    ax.errorbar(distinct_ps/10, means, std, fmt='o', lw=15, alpha=0.3, ecolor='blue')
+    ax.errorbar(distinct_ps/10, means, [means - mins, maxes - means],
+                 fmt='.k', ecolor='black', lw=1)
+    ax.set_xticks(distinct_ps/10)
+    ax.set_xticklabels(distinct_ps)
+    ax.set_xlabel("Number of subprocesses")
+    ax.set_ylabel("Time(s)")
+    ax.set_title(type)
+    plt.savefig(file_name.replace(".csv", "-time.png"))
+
+    x = p_corrects
+    mins = x.min(0)
+    maxes = x.max(0)
+    means = x.mean(0)
+    std = x.std(0)
+
+    # create stacked errorbars:
+    fig, ax1 = plt.subplots()
+    ax1.errorbar(distinct_ps/10, means, std, fmt='o', lw=10, alpha=0.5, ecolor='green')
+    ax1.errorbar(distinct_ps/10, means, [means - mins, maxes - means],
+                 fmt='.k', ecolor='gray', lw=1)
+    ax1.set_xticks(distinct_ps/10)
+    ax1.set_xticklabels(distinct_ps)
+    ax1.set_xlabel("Number of subprocesses")
+    ax1.set_ylabel("Correctness")
+    ax1.set_title(type)
+    plt.savefig(file_name.replace(".csv", "-correctness.png"))
 
 
 
-fig, ax = plt.subplots()
-# draw_plot(np.asarray(p_corrects), 0, "tomato", "white")
 
-bp = ax.boxplot(np.asarray(p_corrects).T, showmeans=True)
-ax.set_xticklabels(distinct_ps)
-ax.set_xlabel("Number of subprocesses")
-ax.set_ylabel("Correctness")
-
-plt.show()
 
 
 
